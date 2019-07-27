46906d60f80eddd94188a1310b69020f97313bc0
Evan Brass <evan-brass@protonmail.com>
2019-07-26 20:32:45 -0700

    Got the ACT blinking

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@
 #![feature(asm)]
 #![no_main]
 
-const MAILBOX: u32 = 0x3f00b880;
-static mut MSG: [u32;8] = [0, 0, 0, 0, 0, 0, 0, 0];
+// const MAILBOX: u32 = 0x3f00b880;
+// static mut MSG: [u32;8] = [0, 0, 0, 0, 0, 0, 0, 0];
 
 fn wait(time: u32) {
 	for _ in 0..time {
@@ -13,37 +13,46 @@ fn wait(time: u32) {
 	}
 }
 
-fn mailbox_send() {
-	let mut status: u32 = 0x80000000u32;
-	while (status & 0x80000000u32) != 0 {
-		status = unsafe{*((MAILBOX + 0x18) as *const u32)};
+// fn mailbox_send() {
+// 	let mut status: u32 = 0x80000000u32;
+// 	while (status & 0x80000000u32) != 0 {
+// 		status = unsafe{*((MAILBOX + 0x18) as *const u32)};
+// 	}
+// 	unsafe{
+// 		*((MAILBOX + 0x20) as *mut u32) = (&MSG as *const u32 as *const u8).offset(8) as u32
+// 	};
+// }
+
+const GPFSEL2: u32 = 0x3f_20_0008;
+fn led_init() {
+	unsafe {
+		*(GPFSEL2 as *mut u32) = 0b001 << 27;
 	}
-	unsafe{
-		*((MAILBOX + 0x20) as *mut u32) = (&MSG as *const u32 as *const u8).offset(8) as u32
-	};
 }
 
-fn turn_on() {
+const GPSET0: u32 = 0x3f_20_001c;
+fn led_off() {
 	unsafe {
-		MSG = [32, 0, 0x00038041, 8, 0, 130, 1, 0];
+		*(GPSET0 as *mut u32) = 0b1 << 29;
 	}
-	mailbox_send();
-
 }
 
-fn turn_off() {
+const GPCLR0: u32 = 0x3f_20_0028;
+// LED is active low
+fn led_on() {
 	unsafe {
-		MSG = [32, 0, 0x00038041, 8, 0, 130, 0, 0];
+		*(GPCLR0 as *mut u32) = 0b1 << 29;
 	}
-	mailbox_send();
 }
 
 fn kernel_entry() -> ! {
-	turn_on();
-	wait(500);
-	turn_off();
-
-	loop {}
+	led_init();
+	loop {
+		led_on();
+		wait(100000);
+		led_off();
+		wait(100000);
+	}
 }
 
 raspi3_boot::entry!(kernel_entry);
