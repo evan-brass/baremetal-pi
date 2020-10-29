1eef60c5d0157511436b8450e6b2314a23b4f548
Evan <evan-brass@protonmail.com>
2020-10-29 13:23:17 -0700

    Get blinking working

--- Makefile
+++
@@ -1,9 +0,0 @@
-TRIPLE= aarch64-unknown-none
-
-.PHONY=all
-
-all: kernel8.img
-
-kernel8.img: src/**/*.*
-	cargo xbuild --release
-	cargo objdump --target $(TRIPLE) -- -disassemble -print-imm-hex ./target/$(TRIPLE)/release/baremetal-pi
--- make.ps1
+++ make.ps1
@@ -1,10 +1,6 @@
 # Clean
 rm .\tftp-root\kernel8.img
-rm .\tftp-root\kernel8
 rm .\tftp-root\kernel8.S
-# Build the project
-cargo xbuild --release
-# Get the image 
-cp .\target\aarch64-unknown-none\release\baremetal-pi .\tftp-root\kernel8
-cargo objcopy -- --strip-all -O binary .\tftp-root\kernel8 .\tftp-root\kernel8.img
-cargo objdump --target aarch64-unknown-none -- -disassemble .\tftp-root\kernel8 > .\tftp-root\kernel8.S
+# Output the binary and assembly
+cargo objcopy --release -- -O binary .\tftp-root\kernel8.img
+cargo objdump --release -- -d > .\tftp-root\kernel8.S
